a30741d3c46f6a9b508ecf9eaa3408e5067fffe5
BreadOne <bbangwon.chung@gmail.com>
2024-08-04 21:19:43 +0900

    스레드 생성 - new Thread()를 통한 생성

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+static void MainThread()
+{
+    while(true)
+        Console.WriteLine("Hello Thread!");
+}
+
+// 쓰레드 생성
+var t = new Thread(MainThread);
+
+//스레드 이름 지정
+t.Name = "Test Thread";
+
+// 쓰레드가 생성될때 기본적으로 Foreground로 생성되는데,
+// 이것을 Background로 변경하면 메인 쓰레드가 종료되면 같이 종료된다.
+t.IsBackground = true;
+
+t.Start();
+Console.WriteLine("Waiting for Thread!");
+
+t.Join();
 Console.WriteLine("Hello, World!");
