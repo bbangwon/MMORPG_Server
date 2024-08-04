2ee71f90e24959c31ebac34aa305d0c9f988f5c7
BreadOne <bbangwon.chung@gmail.com>
2024-08-04 21:30:52 +0900

    쓰레드 생성 - ThreadPool

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,23 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-static void MainThread()
+static void MainThread(object? state)
 {
-    while(true)
+    for (int i = 0; i < 5; i++)
         Console.WriteLine("Hello Thread!");
 }
 
-// 쓰레드 생성
-var t = new Thread(MainThread);
+//쓰레드 풀에서 쓰레드를 가져와서 실행
+ThreadPool.QueueUserWorkItem(MainThread);
 
-//스레드 이름 지정
-t.Name = "Test Thread";
+//// 쓰레드 생성
+//var t = new Thread(MainThread);
 
-// 쓰레드가 생성될때 기본적으로 Foreground로 생성되는데,
-// 이것을 Background로 변경하면 메인 쓰레드가 종료되면 같이 종료된다.
-t.IsBackground = true;
+////스레드 이름 지정
+//t.Name = "Test Thread";
 
-t.Start();
-Console.WriteLine("Waiting for Thread!");
+//// 쓰레드가 생성될때 기본적으로 Foreground로 생성되는데,
+//// 이것을 Background로 변경하면 메인 쓰레드가 종료되면 같이 종료된다.
+//t.IsBackground = true;
 
-t.Join();
-Console.WriteLine("Hello, World!");
+//t.Start();
+//Console.WriteLine("Waiting for Thread!");
+
+//t.Join();
+//Console.WriteLine("Hello, World!");
+Console.ReadLine();
