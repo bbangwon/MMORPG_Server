6bae96cfac730126c269a08b39e9e56b777d33d2
BreadOne <bbangwon.chung@gmail.com>
2024-09-08 16:54:10 +0900

    ReadWriteLock 구현 연습

---
+++ ServerCore/Lock.cs
@@ -0,0 +1,84 @@
+﻿namespace ServerCore
+{
+    //재귀적 락을 허용할지 (No),
+    //Yes일 경우 WriteLock->WriteLock 가능, WriteLock->ReadLock 가능
+    //스핀락 정책(5000번 시도 -> Yield)
+    class Lock
+    {        
+        const int EMPTY_FLAG =  0x00000000;
+        const int WRITE_MASK =  0x7FFF0000;
+        const int READ_MASK =   0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+
+        // [Unused(1)] [WriteThreadId(15)] [ReadCount(16)]
+        // WriteThreadId : WriteLock을 가지고 있는 ThreadId
+        // ReadCount : ReadLock을 가지고 있는 개수
+        int _flag = EMPTY_FLAG;
+        int _writeCount = 0;
+
+        public void WriteLock()
+        {
+            //동일 쓰레드가 WriteLock을 가지고 있는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if(lockThreadId == Environment.CurrentManagedThreadId)
+            {
+                _writeCount++;
+                return;
+            }
+
+            // 아무도 WriteLock or ReadLock을 가지고 있지 않을 때, 경합해서 소유권을 얻는다.
+            int desired = (Environment.CurrentManagedThreadId << 16) & WRITE_MASK;
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    // 시도를 해서 성공하면 리턴
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {
+                        _writeCount = 1;
+                        return;
+                    }
+                }
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            // 동일 쓰레드가 여러번 WriteLock을 걸었을 때, WriteCount만 감소시킨다.
+            int lockCOunt = --_writeCount;
+            if (lockCOunt == 0)
+                Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        }
+
+        public void ReadLock()
+        {
+            // 동일 쓰레드가 WriteLock을 가지고 있는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (lockThreadId == Environment.CurrentManagedThreadId)
+            {
+                Interlocked.Increment(ref _flag);
+                return;
+            }
+
+            // 아무도 WriteLock을 가지고 있지 않을 때, ReadCount를 1 늘림
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    // ReadCount를 1 증가시킨다.
+                    int expected = (_flag & READ_MASK);
+                    if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                        return;
+                }
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnlock()
+        {
+            Interlocked.Decrement(ref _flag);
+        }
+
+    }
+}
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,63 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-lock(_lock)
-{
-    
-}
+using ServerCore;
 
-//SpinLock 사용방법
-//SpinLock는 몇번 시도 하다가 답이 없을 경우에는 중간 중간 yield를 호출한다.
-bool lockTaken = false;
-try
+Task t1 = new Task(() =>
 {
-    //lockTaken이 true가 되면 Enter가 성공한 것이다.
-    _lock2.Enter(ref lockTaken);
-}
-finally
+    for (int i = 0; i < 100000; i++)
+    {
+        _lock.WriteLock();
+        count++;
+        _lock.WriteUnlock();
+    }
+});
+
+Task t2 = new Task(() =>
 {
-    //Exception이 발생하더라도 Exit는 호출되어야 한다.
-    if (lockTaken)
-        _lock2.Exit();
-}
+    for (int i = 0; i < 100000; i++)
+    {
+        _lock.WriteLock();
+        count--;
+        _lock.WriteUnlock();
+    }
+});
+
+t1.Start();
+t2.Start();
+
+Task.WaitAll(t1, t2);
+
+Console.WriteLine(count);
 
 
 partial class Program
 {
-    //내부적으로는 Monitor를 사용한다.
-    static object _lock = new object();
-    static SpinLock _lock2 = new SpinLock();
-
-    //많이 느림
-    //같은 프로그램이 아니더라도 순서를 맞추는 동기화가 가능함
-    //MMORPG에서는 거의 사용하지 않음
-    static Mutex _lock3 = new Mutex();
-
-    //[] [] [] [] []
-    class Reward
-    {
-
-    }
-
-    static ReaderWriterLockSlim _lock4 = new ReaderWriterLockSlim();
-
-    //리워드를 찾아서 반환해주는
-    // 99.99999%
-    static Reward? GetRewardById(int id)
-    {
-        //아무도 WriteLock을 가지고 있지 않다면 스레드들이 동시에 읽기를 할 수 있다.
-        //마치 Lock을 걸지 않은 것처럼 동작한다.
-        _lock4.EnterReadLock();
-
-        _lock4.ExitReadLock();
-        return null;
-    }
-
-    // 0.00001%
-    static void AddReward(Reward reward)
-    {
-        _lock4.EnterWriteLock();
-
-        _lock4.ExitWriteLock();
-    }
+    static volatile int count = 0;
+    static Lock _lock = new Lock();
 }
 
